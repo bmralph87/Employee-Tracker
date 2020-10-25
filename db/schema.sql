90e9b381c4602d0bed45ead6fb8d9d7761944aae
Britt Ralph <brittney.mr22@gmail.com>
2020-10-25 16:40:16 -0400

    modified

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,31 @@
-DROP DATABASE IF EXISTS employee_DB;
-CREATE DATABASE employee_DB;
-USE employee_DB;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,4) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
     PRIMARY KEY (id),
-	FOREIGN KEY (department_id) REFERENCES department(id)
+	FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INT AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
   manager_id INT,
  PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
 
 SET @@auto_increment_increment=1;
